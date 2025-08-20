d61c7d4ce8a7b077f83f3ec88cafc1748747c8e5
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2025-08-20 09:43:34 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  default = "default"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name for EC2"
+  type        = string
+  default     = "my-ec2-key"
+}
